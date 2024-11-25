037ce96d0f5e4424f6688024f4c6e2978d2c712c
Yasin Gündüz <yasin.guenduez1@endress.com>
2024-11-25 12:58:34 +0100

    cpu load service

--- src/main_server.rs
+++ src/main_server.rs
@@ -1,15 +1,34 @@
-//! Serves a Bluetooth GATT echo server.
+use systemstat::{Platform, System};
 
 /// Service UUID for GATT example.
 ///
 const SERVICE_ID: &str = "FD2B4448-AA0F-4A15-A62F-EB0BE77A0000";
 
 /// Characteristic UUID for GATT example.
-const CHARACTERISTIC_UUID: uuid::Uuid = uuid::Uuid::from_u128(0xF00DC0DE00002);
+// const CHARACTERISTIC_UUID: uuid::Uuid = uuid::Uuid::from_u128(0xF00DC0DE00002);
 
 /// RSSI Characteristic UUID for GATT example.
-const RSSI_CHARACTERISTIC_UUID: uuid::Uuid = uuid::Uuid::from_u128(0xFEEDC0DE00003);
+// const RSSI_CHARACTERISTIC_UUID: uuid::Uuid = uuid::Uuid::from_u128(0xFEEDC0DE00003);
 
+/// Temperature
+const TEMPERATURE: uuid::Uuid = uuid::Uuid::from_u128(0xFEEDC0DE00001);
+
+/// CPU LOAD
+const CPU_LOAD: uuid::Uuid = uuid::Uuid::from_u128(0xFEEDC0DE00002);
+
+/// RAM USAGE
+const RAM_USAGE: uuid::Uuid = uuid::Uuid::from_u128(0xFEEDC0DE00003);
+
+/// Uptime
+const UPTIME: uuid::Uuid = uuid::Uuid::from_u128(0xFEEDC0DE00004);
+
+/*
+const uuids = {
+// '00000000-0000-0000-0000-fd2bcccb0001': 'temperature',// 34 C
+// '00000000-0000-0000-0000-fd2bcccb0002': 'CPU', // 45
+// '00000000-0000-0000-0000-fd2bcccb0003': 'RAM', // 110/926MB
+// 'readValue fd2b4448-aa0f-4a15-a62f-eb0be77a0004': 'Uptime'}
+ */
 use bluer::{
     adv::Advertisement,
     gatt::{
@@ -21,13 +40,10 @@ use bluer::{
         CharacteristicReader, CharacteristicWriter,
     },
 };
-use futures::{future, pin_mut, StreamExt};
+use futures::{pin_mut, StreamExt};
 use std::str::FromStr;
 use std::time::Duration;
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    time::sleep,
-};
+use tokio::{io::AsyncWriteExt, time, time::sleep};
 
 #[tokio::main]
 async fn main() -> bluer::Result<()> {
@@ -60,13 +76,9 @@ async fn main() -> bluer::Result<()> {
             uuid: service_uuid,
             primary: true,
             characteristics: vec![
+                // CPU Load characteristic
                 Characteristic {
-                    uuid: CHARACTERISTIC_UUID,
-                    write: Some(CharacteristicWrite {
-                        write_without_response: true,
-                        method: CharacteristicWriteMethod::Io,
-                        ..Default::default()
-                    }),
+                    uuid: CPU_LOAD,
                     notify: Some(CharacteristicNotify {
                         notify: true,
                         method: CharacteristicNotifyMethod::Io,
@@ -75,16 +87,17 @@ async fn main() -> bluer::Result<()> {
                     control_handle: char_handle,
                     ..Default::default()
                 },
-                // Add RSSI Characteristic
-                Characteristic {
-                    uuid: RSSI_CHARACTERISTIC_UUID,
-                    notify: Some(CharacteristicNotify {
-                        notify: true,
-                        method: CharacteristicNotifyMethod::Io,
-                        ..Default::default()
-                    }),
-                    ..Default::default()
-                },
+                // // CPU Temperature
+                // Characteristic {
+                //     uuid: TEMPERATURE,
+                //     notify: Some(CharacteristicNotify {
+                //         notify: true,
+                //         method: CharacteristicNotifyMethod::Io,
+                //         ..Default::default()
+                //     }),
+                //     control_handle: char_handle,
+                //     ..Default::default()
+                // },
             ],
             ..Default::default()
         }],
@@ -94,60 +107,31 @@ async fn main() -> bluer::Result<()> {
 
     println!("GATT Service Ready - Serving");
 
-    let mut read_buf = Vec::new();
-    let mut reader_opt: Option<CharacteristicReader> = None;
-    let mut writer_opt: Option<CharacteristicWriter> = None;
+    let mut cpu_load_writer_opt: Option<CharacteristicWriter> = None;
     pin_mut!(char_control);
 
+    let sys = System::new();
+
     loop {
-        let rssi_value: i32 = 42;
-        let rssi_data = rssi_value.to_le_bytes();
-
-        if let Some(writer) = &mut writer_opt {
-            writer.write_all(&rssi_data).await?;
-        }
-
         tokio::select! {
             evt = char_control.next() => {
                 match evt {
-                    Some(CharacteristicControlEvent::Write(req)) => {
-                        println!("Accepting write request event with MTU {}", req.mtu());
-                        read_buf = vec![0; req.mtu()];
-                        reader_opt = Some(req.accept()?);
-                    },
                     Some(CharacteristicControlEvent::Notify(notifier)) => {
                         println!("Accepting notify request event with MTU {}", notifier.mtu());
-                        writer_opt = Some(notifier);
+                        cpu_load_writer_opt = Some(notifier);
                     },
                     None => break,
-                }
+                _ => {break}}
             },
-            read_res = async {
-                match &mut reader_opt {
-                    Some(reader) if writer_opt.is_some() => reader.read(&mut read_buf).await,
-                    _ => future::pending().await,
-                }
-            } => {
-                match read_res {
-                    Ok(0) => {
-                        println!("Read stream ended");
-                        reader_opt = None;
-                    }
-                    Ok(n) => {
-                        let value = read_buf[..n].to_vec();
-                        println!("Echoing {} bytes: {:x?} ... {:x?}", value.len(), &value[0..4.min(value.len())], &value[value.len().saturating_sub(4) ..]);
-                        if value.len() < 512 {
-                            println!();
-                        }
-                        if let Err(err) = writer_opt.as_mut().unwrap().write_all(&value).await {
-                            println!("Write failed: {}", &err);
-                            writer_opt = None;
-                        }
-                    }
-                    Err(err) => {
-                        println!("Read stream error: {}", &err);
-                        reader_opt = None;
-                    }
+            _ = time::sleep(Duration::from_secs(1)) => {
+                let cpu_load = sys.cpu_load_aggregate()?.done()?;
+                let system_cpu_load = cpu_load.system;
+                
+                println!("CPU LOAD is: {system_cpu_load}");
+
+                if let Some(writer) = &mut cpu_load_writer_opt {
+                    writer.write_f32(system_cpu_load).await?;
+                    println!("Updated CPU load characteristic: {:.2}%", system_cpu_load);
                 }
             }
         }
