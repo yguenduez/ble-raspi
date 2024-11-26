c3cf5a641d17f8859dffe198749cfe84b25c10bd
Yasin Gündüz <yasin.guenduez1@endress.com>
2024-11-26 09:26:42 +0100

    write_all should write the whole buffer

--- src/main_server.rs
+++ src/main_server.rs
@@ -179,7 +179,8 @@ async fn main() -> bluer::Result<()> {
                 }
                 if let Some(writer) = &mut memory_writer_opt {
                     let usage = format!("Memory Usage is: {}/{}", memory_usage.total.as_u64() - memory_usage.free.as_u64(), memory_usage.total.as_u64());
-                    writer.send(&usage.clone().into_bytes()).await?;
+                    writer.write_all(&usage.clone().into_bytes()).await?;
+                    writer.flush().await?;
                     println!("Updated Memory usage: {usage}");
                 }
             }
